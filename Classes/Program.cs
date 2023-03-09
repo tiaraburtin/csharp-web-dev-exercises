ae6d75c96e0ef8a59dcdf0f5a926ba200d22e230
Tiara <tburtin1019@gmail.com>
2023-03-09 15:38:43 -0600

    made classes

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,1 +1,6 @@
 ﻿// TODO: Instantiate your objects and test your exercise solutions with print statements here.
+using Classes;
+
+Student whateverTheNewName = new Student("Tiara", 333, 1, 4.0);
+
+Console.WriteLine(whateverTheNewName.Name);
